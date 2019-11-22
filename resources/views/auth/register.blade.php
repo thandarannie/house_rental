4093190a59674ec0e9e3720043567e960a23bf35
Thandar Tun <thandarannie12@gmail.com>
2019-11-22 17:13:58 +0630

    database table updated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -38,7 +38,35 @@
                                 @enderror
                             </div>
                         </div>
+                        <div class="form-group row">
+                            <label for="nrc" class="col-md-4 col-form-label text-md-right">{{ __('NRC') }}</label>
 
+                            <div class="col-md-6">
+                                <input id="nrc" type="text" class="form-control @error('nrc') is-invalid @enderror" name="nrc" value="{{ old('nrc') }}" required autocomplete="nrc">
+
+                                @error('nrc')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="image" class="col-md-4 col-form-label text-md-right">{{ __('Image') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="image" type="file" name="email"  required autocomplete="image">
+
+                            </div>
+                        </div>
+                            <!-- Radio -->
+                            <!-- <div class="form-group row">
+                                <label for="type" class="col-md-4 col-form-label text-md-right">{{ __('User Type') }}</label>
+                                 <div class="col-md-6">
+                                     <input type="radio" name="usertype" value="User">User
+                                    <input type="radio" name="usertype" value="Owner">Owner
+                                 </div>
+                            </div> -->
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
@@ -52,7 +80,7 @@
                                 @enderror
                             </div>
                         </div>
-
+                        
                         <div class="form-group row">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
