14c7dd145c5fe75d6e4f3e6751e696789d05ff57
nanyuyumaw <yuyumaw1236@gmail.com>
2019-11-23 11:17:19 +0630

    nav.blade fixed by nanyuyumaw

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,8 @@
-@extends('layouts.app')
+@extends('template')
 
 @section('content')
-<div class="container">
+<br><br>
+<div class="container mt-5 mb-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
