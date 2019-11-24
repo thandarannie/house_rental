38fde6fbe9190a1da273669f010b9409d5d268cc
Than Toe Aung <Than Toe Aung>
2019-11-24 11:59:15 +0630

    about page by thantoeaung

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -15,46 +15,64 @@
   </div>
 </section>
 
-<section class="ftco-section ftco-no-pb">
+<section class="ftco-section ftco-animate">
   <div class="container">
     <div class="row">
-      <div class="col-md-6 img img-3 d-flex justify-content-center align-items-center" style="background-image: url(frontend/images/house_rent.jpg);">
-      </div>
+      <!-- <div class="col-md-6 img img-3 d-flex justify-content-center align-items-center" style="background-image: url(frontend/images/house_rent.jpg);" alt="Colorlib Template"> -->
+        <img src="frontend/images/house_rent.jpg" class="col-md-6 img img-3 d-flex img-fluid" alt="Responsive image" >
+      
       <div class="col-md-6 wrap-about pl-md-5 ftco-animate">
         <div class="heading-section">
-          <h2 class="mb-4">Welcome To House Rental Mandalay Agency</h2>
+          <h2 class="mb-6">Welcome To House Rental Mandalay Agency</h2>
 
-          <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-          <p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country.</p>
+          <p>Being live, we need safety buildings to live happily.So people want to have a good house in safety environment.Therefore,we create our agency-<b>House Rental Mandalay Agency</b> for customers.</p>
+          <p>Renting a house is a much better option than purchasing a house
+                and it is the clear choice for my family in today’s changing world. If you carefully investigate buying and renting options, you will conclude that renting is the most beneficial way to ensure you get the most for your money. The cost of owning a house has risen over the last several years creating uncertainty in the housing market.</p>
         </div>
       </div>
     </div>
   </div>
-</section>
 
+
+
+</section>
 
 
 <section class="ftco-section">
   <div class="container">
     <div class="row">
-      <div class="col-md-4">
-        <h3 style="font-weight: 600; font-size: 20px;">Our Mission</h3>
-        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+      <div class="col-md-6">
+        <h3 style="font-weight: 600; font-size: 20px;">The advantages:</h3>
+        <ul>
+          <li>It is usually cheaper to rent than buy.</li>
+
+          <li>You can be more flexible when it comes to moving to another city/country.</li>
+
+          <li>You are not responsible for maintenance costs.</li>
+        </ul>
+        <h3 style="font-weight: 600; font-size: 20px;">The disadvantages:</h3>
+
+        <ul>
+          <li>You may face some misunderstandings with landlord.</li>
+
+          <li>You are not allowed to make improvements and decorate the property without landlord’s approval.</li>
+
+          <li>You may not be able to renew your lease on the same or similar terms and conditions.</li>
+        </ul>
       </div>
-      <div class="col-md-4">
-        <h3 style="font-weight: 600; font-size: 20px;">Our Vission</h3>
-        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-      </div>
-      <div class="col-md-4">
+
+      
+
+      <div class="col-md-6">
         <h3 style="font-weight: 600; font-size: 20px;">Our Value</h3>
-        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+        <p>By using our website,we will serve to save time and loss of costs for customers instead of looking for houses of the outside environments, and give pleasure and satisfy for customers.For owners,they can be easily posts of their houses on my website and easily sell them to customers.Our agency is a good agency between customers and owners.</p>
       </div>
+
+    
     </div>
   </div>
+  
 </section>
-
-
-
 
 
 @endsection
